ad57fbd10ce3b29e2018d92dedef145e51e68a50
Rapina <rapina.sv04@gmail.com>
2025-05-14 20:34:55 -0300

    Bug Fix Loading Screen

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,8 @@
-const aspectRatio = window.innerWidth / window.innerHeight;
 const userAgent = navigator.userAgent;
-var deviceRotation;
 var device;
+var aspectRatio;
+var deviceOrientation;
 
-if (aspectRatio >= 1) {
-    deviceRotation = "Landscape";
-} else {
-    deviceRotation = "Portrait";
-}
 if (/Mobi|Android/i.test(userAgent)) {
   device = "Mobile";
 } else if (/Tablet|iPad/i.test(userAgent)) {
@@ -16,40 +11,58 @@ if (/Mobi|Android/i.test(userAgent)) {
   device = "Desktop";
 }
 
+const loading = {};
+loading.screen = document.getElementById("loading");
+loading.icon = document.getElementById("icon");
+loading.errorMessage = document.getElementById("error-message");
 
+function orientationCheck() {
+    setTimeout(() => {
+        
+        // Detect orientation
+        aspectRatio = window.innerWidth / window.innerHeight;
+        if (aspectRatio >= 1) {
+            deviceOrientation = "Landscape";
+        } else {
+            deviceOrientation = "Portrait";
+        }
+        // Reset Loading Screen
+        loading.screen.style.animation = "";
+        loading.icon.style.animation = "rotate 1s ease-in-out infinite forwards";
+        loading.icon.style.display = "block";
+        loading.errorMessage.innerHTML = "";
+        loading.errorMessage.style.display = "none";
+        // Apply result
+        if (device == "Desktop" && deviceOrientation == "Landscape") {
+            loading.screen.style.animation = "loading-successful 1s forwards";
+            loading.icon.style.animation = "loading-successful-icon 1s forwards";
+    
+        } else if (device  == "Desktop" && deviceOrientation == "Portrait") {
+            loading.errorMessage.style.display = "flex";
+            loading.icon.style.display = "none";
+            loading.errorMessage.innerHTML = `
+                <p>Please turn your device</p>
+                <img src="assets/menu/loadingerrorpc.png">
+                <p class="error-minitext">Why the f would you use your monitor in portrait?</p>
+            `;
+    
+        } else if (device == "Mobile" && deviceOrientation == "Landscape") {
+          loading.screen.style.animation = "loading-successful 1s forwards";
+          loading.icon.style.animation = "loading-successful-icon 1s forwards";
+          
+        } else if (device == "Mobile" && deviceOrientation == "Portrait") {
+            loading.errorMessage.style.display = "flex";
+            loading.icon.style.display = "none";
+            loading.errorMessage.innerHTML = `
+                <p>Please turn your device</p>
+                <img src="assets/menu/loadingerrormobile.png">
+            `;
+        }
+        
+    }, 100);
 
-// Loading
-const loading = document.getElementById("loading");
+}
 setTimeout(() => {
-
-    if (device == "Desktop" && deviceRotation == "Landscape") {
-        loading.style.animation = "loading-successful 1s forwards";
-        document.getElementById("icon").style.animation = "loading-successful-icon 1s forwards";
-
-    } else if (device  == "Desktop" && deviceRotation == "Portrait") {
-        let errorMessage = document.createElement("div");
-        errorMessage.id = "error-message";
-        errorMessage.innerHTML = `
-            <p>Please turn your device</p>
-            <img src="assets/menu/loadingerrorpc.png">
-            <p class="error-minitext">Why the f would you use your monitor in portrait?</p>
-        `;
-        document.getElementById("icon").remove();
-        loading.appendChild(errorMessage);
-
-    } else if (device == "Mobile" && deviceRotation == "Landscape") {
-      loading.style.animation = "loading-successful 1s forwards";
-      document.getElementById("icon").style.animation = "loading-successful-icon 1s forwards";
-      
-    } else if (device == "Mobile" && deviceRotation == "Portrait") {
-        let errorMessage = document.createElement("div");
-        errorMessage.id = "error-message";
-        errorMessage.innerHTML = `
-            <p>Please turn your device</p>
-            <img src="assets/menu/loadingerrormobile.png">
-        `;
-        document.getElementById("icon").remove();
-        loading.appendChild(errorMessage);
-    }
-
+    orientationCheck()
+    window.addEventListener("orientationchange", orientationCheck);
 }, 1000);
